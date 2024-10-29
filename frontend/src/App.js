f1b2802f93e3a5cd232050642143a87179a3b5b0
mso872 <mso872@gmail.com>
2024-10-29 06:45:36 -0700

    Delete hero component and remove app import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import CopyGenerator from './components/CopyGenerator';
 import Nav from './components/Nav';
-// eslint-disable-next-line
-import Hero from './components/Hero';
 import './App.css'
 
 const App = () => {
     return (
         <div id="app">
             <Nav />
-            {/* <Hero /> */}
             <CopyGenerator />
         </div>
     );
