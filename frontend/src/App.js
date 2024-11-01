099d282428e128755ba8a0c369eab887ea113959
mso872 <mso872@gmail.com>
2024-11-01 08:01:00 -0700

    Import hero component to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import CopyGenerator from './components/CopyGenerator';
 import Nav from './components/Nav';
 import './App.css'
+import HomeHero from './components/HomeHero';
 
 const App = () => {
     return (
         <div id="app">
             <Nav />
+            <HomeHero />
             <CopyGenerator />
         </div>
     );
