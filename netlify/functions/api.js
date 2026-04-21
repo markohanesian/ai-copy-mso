836723104306bff6a6ce7631dd3ec22fd633082c
Mark <mso872@gmail.com>
2026-04-21 10:45:59 -0700

    Add dotenv support to netlify function

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const serverless = require('serverless-http');
+require('dotenv').config();
 
 const app = express();
 
